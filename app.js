17b5e47d543d8038cbd8ff955e852fdb598645a4
FlorBera <florencia.berardi@hotmail.com>
2022-12-14 20:50:51 -0300

    deploy

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const path = require("path");
 
 const app = express();
 
+let port = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Servidor corriendo en el puerto 3000");
-});
+app.listen (port, () => (console.log("Servidor corriendo en el puerto 3000")));
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/home.html"));
